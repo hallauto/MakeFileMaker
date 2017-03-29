0e4703b1e41e01580e48ad6dc0e081d6d590cd92
kimchangyu <cdit2464@gmail.com>
2017-03-29 13:16:31 +0900

    MakeFileMaker 프로젝트 시작
    MakeFile 만들기 귀찮다는 마음이 이런 끔찍한 혼종을 만드네요.
    MakeFile 만드는 프로그램 MakeFileMaker는
    리눅스에서도 잘 돌기를 바라며 자바로 만들어집니다.
    파이썬이나 웹앱이요? 버려버려

---
+++ src/data_for_make/MacroMaker.java
@@ -0,0 +1,11 @@
+package data_for_make;
+
+public class MacroMaker extends StringDescriptor {
+
+	public MacroMaker() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
---
+++ src/gui_java/Main_window.java
@@ -0,0 +1,48 @@
+package gui_java;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+
+public class Main_window {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main_window window = new Main_window();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Main_window() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		frame.getContentPane().add(lblNewLabel, BorderLayout.NORTH);
+	}
+
+}
