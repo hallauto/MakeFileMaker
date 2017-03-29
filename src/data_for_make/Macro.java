f40b47460141c0f51957e81d4ac569de5f8829dc
kimchangyu <cdit2464@gmail.com>
2017-03-29 18:40:04 +0900

    매크로 클래스 제작중
    매크로클래스를 제작중입니다. 근데 이게 매크로 맞나요?

--- src/data_for_make/Macro.java
+++ src/data_for_make/Macro.java
@@ -1,5 +1,27 @@
 package data_for_make;
 
 public class Macro {
+	protected String Ori_string;
+	protected String Changed_name;
+	
+	
+	
+	public Macro(String Ori_string, String Changed_name) {
+		this.Ori_string = Ori_string;
+		this.Changed_name = Changed_name;
+	}
+	
+	public String getTotalToken() throws Exception
+	{
+		String TotalToken = "$";
+		
+		if (Ori_string.isEmpty() || Changed_name.isEmpty())
+		{
+			throw new Exception("Ori_string is empty");
+		}
+		
+		return TotalToken;
+	}
+	
 
 }
